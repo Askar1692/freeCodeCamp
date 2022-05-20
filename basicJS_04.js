556b365bf2ffd99859584e8458f2656dc82e0026
Askar1692 <askar.kusemisov@gmail.com>
2022-05-20 23:58:47 +0600

    added new file

---
+++ basicJS_04.js
@@ -0,0 +1,10 @@
+function trueOrFalse(wasThatTrue) {
+
+    if (wasThatTrue) {
+        return "Yes, that was true";
+    } else {
+        return "It was false";
+    }
+}
+
+console.log(trueOrFalse(true));
